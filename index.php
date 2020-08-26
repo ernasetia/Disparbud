a7396fd554e6a172dc903677582c0841fb458f1b
erna setia <ernasetia168@gmail.com>
2020-08-26 09:36:52 +0700

    helllo

--- index.php
+++ index.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>CRUD Petani Kode</title>
-    <link rel="icon" href="http://www.petanikode.com/favicon.ico" />
+    <title>Pendataan Santri</title>
+    <link rel="icon" href="htdocs/pemweb/m6/icons8-squared-menu-25.png"/>
+    <link rel="stylesheet" href="modul6.css">
 </head>
 <body>
     <br>
     <center><h2>APLIKASI PENDATAAN SANTRI TPQ MIFTAKHUL ULUM</h2></center>
 <?php
 // --- koneksi ke database
-$koneksi = mysqli_connect("localhost","root","","pertanian") or die (mysqli_error());
+$koneksi = mysqli_connect("localhost","root","","webtpq7") or die (mysqli_error());
 
 // --- Fngsi tambah data (Create)
 function tambah($koneksi){
     
     if (isset($_POST['btn_simpan'])){
-        $id = time();
-        $nm_tanaman = $_POST['nm_tanaman'];
-        $hasil = $_POST['hasil'];
-        $lama = $_POST['lama'];
-        $tgl_panen = $_POST['tgl_panen'];
+        $id_santri = time();
+        $nm_santri = $_POST['nm_santri'];
+        $jk = $_POST['jk'];
+        $tingkat = $_POST['tingkat'];
+        $tgl_lahir = $_POST['tgl_lahir'];
         
-        if(!empty($nm_tanaman) && !empty($hasil) && !empty($lama) && !empty($tgl_panen)){
-            $sql = "INSERT INTO tabel_panen (id,nama_tanaman, hasil_panen, lama_tanam, tanggal_panen) VALUES(".$id.",'".$nm_tanaman."','".$hasil."','".$lama."','".$tgl_panen."')";
+        if(!empty($nm_santri) && !empty($jk) && !empty($tingkat) && !empty($tgl_lahir)){
+            $sql = "INSERT INTO data_santri (id_santri,nama_santri,jenis_kelamin, tingkat_santri, tanggal_lahir) VALUES (".$id_santri.",'".$nm_santri."','".$jk."','".$tingkat."','".$tgl_lahir."')";
             $simpan = mysqli_query($koneksi, $sql);
             if($simpan && isset($_GET['aksi'])){
                 if($_GET['aksi'] == 'create'){
-                    header('location: index.php');
+                    header('location: indexx.php');
                 }
             }
         } else {
@@ -37,10 +38,10 @@ function tambah($koneksi){
         <form action="" method="POST">
             <fieldset>
                 <legend><h2>Tambah data</h2></legend>
-                <label>Nama tanaman <input type="text" name="nm_tanaman" /></label> <br>
-                <label>Hasil panen <input type="number" name="hasil" /> kg</label><br>
-                <label>Lama tanam <input type="number" name="lama" /> bulan</label> <br>
-                <label>Tanggal panen <input type="date" name="tgl_panen" /></label> <br>
+                <label>Nama Santri <input type="text" name="nm_santri" /></label> <br>
+                <label>Jenis Kelamin <input type="text" name="jk" /></label><br>
+                <label>Tingkat Santri <input type="text" name="tingkat" /></label> <br>
+                <label>Tanggal Lahir <input type="date" name="tgl_lahir" /></label> <br>
                 <br>
                 <label>
                     <input type="submit" name="btn_simpan" value="Simpan"/>
@@ -55,33 +56,33 @@ function tambah($koneksi){
 // --- Tutup Fngsi tambah data
 // --- Fungsi Baca Data (Read)
 function tampil_data($koneksi){
-    $sql = "SELECT * FROM tabel_panen";
+    $sql = "SELECT * FROM data_santri";
     $query = mysqli_query($koneksi, $sql);
     
     echo "<fieldset>";
-    echo "<legend><h2>Data Panen</h2></legend>";
+    echo "<legend><h2>Data Santri</h2></legend>";
     
     echo "<table border='1' cellpadding='10'>";
     echo "<tr>
-            <th>ID</th>
-            <th>Nama Tanaman</th>
-            <th>Hasil Panen</th>
-            <th>Lama Tanam</th>
-            <th>Tanggal Panen</th>
-            <th>Tindakan</th>
+            <th>Id Santri</th>
+            <th>Nama Santri</th>
+            <th>Jenis Kelamin</th>
+            <th>Tingkat Santri</th>
+            <th>Tanggal Lahir</th>
+            <th>Akses</th>
           </tr>";
     
     while($data = mysqli_fetch_array($query)){
         ?>
             <tr>
-                <td><?php echo $data['id']; ?></td>
-                <td><?php echo $data['nama_tanaman']; ?></td>
-                <td><?php echo $data['hasil_panen']; ?> Kg</td>
-                <td><?php echo $data['lama_tanam']; ?> bulan</td>
-                <td><?php echo $data['tanggal_panen']; ?></td>
+                <td><?php echo $data['id_santri']; ?></td>
+                <td><?php echo $data['nama_santri']; ?></td>
+                <td><?php echo $data['jenis_kelamin']; ?></td>
+                <td><?php echo $data['tingkat_santri']; ?></td>
+                <td><?php echo $data['tanggal_lahir']; ?></td>
                 <td>
-                    <a href="index.php?aksi=update&id=<?php echo $data['id']; ?>&nama=<?php echo $data['nama_tanaman']; ?>&hasil=<?php echo $data['hasil_panen']; ?>&lama=<?php echo $data['lama_tanam']; ?>&tanggal=<?php echo $data['tanggal_panen']; ?>">Ubah</a> |
-                    <a href="index.php?aksi=delete&id=<?php echo $data['id']; ?>">Hapus</a>
+                    <a href="indexx.php?aksi=update&id_santri=<?php echo $data['id_santri']; ?>&nama=<?php echo $data['nama_santri']; ?>&jenkel=<?php echo $data['jenis_kelamin']; ?>&tingkat=<?php echo $data['tingkat_santri']; ?>&tanggal=<?php echo $data['tanggal_lahir']; ?>">Ubah</a> |
+                    <a href="indexx.php?aksi=delete&id_santri=<?php echo $data['id_santri']; ?>">Hapus</a>
                 </td>
             </tr>
         <?php
@@ -90,23 +91,24 @@ function tampil_data($koneksi){
     echo "</fieldset>";
 }
 // --- Tutup Fungsi Baca Data (Read)
+
 // --- Fungsi Ubah Data (Update)
 function ubah($koneksi){
     // ubah data
     if(isset($_POST['btn_ubah'])){
-        $id = $_POST['id'];
-        $nm_tanaman = $_POST['nm_tanaman'];
-        $hasil = $_POST['hasil'];
-        $lama = $_POST['lama'];
-        $tgl_panen = $_POST['tgl_panen'];
+        $id_santri = $_POST['id_santri'];
+        $nm_santri = $_POST['nm_santri'];
+        $jk = $_POST['jk'];
+        $tingkat = $_POST['tingkat'];
+        $tgl_lahir = $_POST['tgl_lahir'];
         
-        if(!empty($nm_tanaman) && !empty($hasil) && !empty($lama) && !empty($tgl_panen)){
-            $perubahan = "nama_tanaman='".$nm_tanaman."',hasil_panen=".$hasil.",lama_tanam=".$lama.",tanggal_panen='".$tgl_panen."'";
-            $sql_update = "UPDATE tabel_panen SET ".$perubahan." WHERE id=$id";
+        if(!empty($nm_santri) && !empty($jk) && !empty($tingkat) && !empty($tgl_lahir)){
+            $perubahan = "nama_santri=".$nm_santri.",jenis_kelamin=".$jk.",tingkat_santri=".$tingkat.",tanggal_lahir='".$tgl_lahir."'";
+            $sql_update = "UPDATE data_santri SET ".$perubahan." WHERE id_santri=$id_santri";
             $update = mysqli_query($koneksi, $sql_update);
             if($update && isset($_GET['aksi'])){
                 if($_GET['aksi'] == 'update'){
-                    header('location: index.php');
+                    header('location: indexx.php');
                 }
             }
         } else {
@@ -115,23 +117,23 @@ function ubah($koneksi){
     }
     
     // tampilkan form ubah
-    if(isset($_GET['id'])){
+    if(isset($_GET['id_santri'])){
         ?>
-            <a href="index.php"> &laquo; Home</a> | 
-            <a href="index.php?aksi=create"> (+) Tambah Data</a>
+            <a href="indexx.php"> &laquo; Home</a> | 
+            <a href="indexx.php?aksi=create"> (+) Tambah Data</a>
             <hr>
             
             <form action="" method="POST">
             <fieldset>
                 <legend><h2>Ubah data</h2></legend>
-                <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>"/>
-                <label>Nama tanaman <input type="text" name="nm_tanaman" value="<?php echo $_GET['nama'] ?>"/></label> <br>
-                <label>Hasil panen <input type="number" name="hasil" value="<?php echo $_GET['hasil'] ?>"/> kg</label><br>
-                <label>Lama tanam <input type="number" name="lama" value="<?php echo $_GET['lama'] ?>"/> bulan</label> <br>
-                <label>Tanggal panen <input type="date" name="tgl_panen" value="<?php echo $_GET['tanggal'] ?>"/></label> <br>
+                <input type="hidden" name="id_santri" value="<?php echo $_GET['id_santri'] ?>"/>
+                <label>Nama Santri <input type="text" name="nm_santri" value="<?php echo $_GET['nama'] ?>"/></label> <br>
+                <label>Jenis Kelamin <input type="text" name="jk" value="<?php echo $_GET['jenkel'] ?>"/></label><br>
+                <label>Tingkat Santri <input type="text" name="tingkat" value="<?php echo $_GET['tingkat'] ?>"/></label> <br>
+                <label>Tanggal Lahir <input type="date" name="tgl_lahir" value="<?php echo $_GET['tanggal'] ?>"/></label> <br>
                 <br>
                 <label>
-                    <input type="submit" name="btn_ubah" value="Simpan Perubahan"/> atau <a href="index.php?aksi=delete&id=<?php echo $_GET['id'] ?>"> (x) Hapus data ini</a>!
+                    <input type="submit" name="btn_ubah" value="Simpan Perubahan"/> atau <a href="indexx.php?aksi=delete&id_santri=<?php echo $_GET['id_santri'] ?>"> (x) Hapus data ini</a>!
                 </label>
                 <br>
                 <p><?php echo isset($pesan) ? $pesan : "" ?></p>
@@ -143,16 +145,17 @@ function ubah($koneksi){
     
 }
 // --- Tutup Fungsi Update
+
 // --- Fungsi Delete
 function hapus($koneksi){
-    if(isset($_GET['id']) && isset($_GET['aksi'])){
-        $id = $_GET['id'];
-        $sql_hapus = "DELETE FROM tabel_panen WHERE id=" . $id;
+    if(isset($_GET['id_santri']) && isset($_GET['aksi'])){
+        $id_santri = $_GET['id_santri'];
+        $sql_hapus = "DELETE FROM data_santri WHERE id_santri=" . $id_santri;
         $hapus = mysqli_query($koneksi, $sql_hapus);
         
         if($hapus){
             if($_GET['aksi'] == 'delete'){
-                header('location: index.php');
+                header('location: indexx.php');
             }
         }
     }
@@ -164,7 +167,7 @@ function hapus($koneksi){
 if (isset($_GET['aksi'])){
     switch($_GET['aksi']){
         case "create":
-            echo '<a href="index.php"> &laquo; Home</a>';
+            echo '<a href="indexx.php"> &laquo; Home</a>';
             tambah($koneksi);
             break;
         case "read":
@@ -178,7 +181,7 @@ if (isset($_GET['aksi'])){
             hapus($koneksi);
             break;
         default:
-            echo "<h3>Aksi <i>".$_GET['aksi']."</i> tidaka ada!</h3>";
+            echo "<h3>Aksi <i>".$_GET['aksi']."</i> tidak ada!</h3>";
             tambah($koneksi);
             tampil_data($koneksi);
     }
